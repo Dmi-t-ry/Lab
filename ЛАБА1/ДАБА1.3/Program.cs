b1252e8af5a0416cc219b0df29612aa50d3b697c
Dmi-t-ry <93443687+Dmi-t-ry@users.noreply.github.com>
2023-04-19 15:02:50 +0300

    Add files via upload

---
+++ ЛАБА1/ДАБА1.3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Text.Json;
+
+bool ContinueProgramExecutionRequest()
+{
+    Console.WriteLine("Retry the program?(True / False)");
+    bool response = false;
+    try
+    {
+        response = bool.Parse(Console.ReadLine());
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Invalid value entered. Press any key to exit the program.");
+        Console.ReadKey();
+    }
+    Console.Clear();
+    return response;
+}
+
+void FindCountOfPositive(List<int> listOfNumbers)
+{
+    Console.WriteLine($"Count of positive:{(from n in listOfNumbers where n > 0 select n).Count()}"); 
+}
+void FindSumOfOdd(List<int> listOfNumbers)
+{
+    Console.WriteLine($"Sum of odd:{(from n in listOfNumbers where n%2!=0 & n>0 select n).Sum()}");
+}
+
+void ProgramExecution()
+{
+    List<int> listOfNumbers = new List<int>()
+    {
+        1,
+        -13,
+        42,
+        3,
+        -2,
+        5,
+        7,
+        4,
+        3
+    };
+    FindCountOfPositive(listOfNumbers);
+    FindSumOfOdd(listOfNumbers);
+}
+
+do
+{
+    ProgramExecution();
+}
+while (ContinueProgramExecutionRequest() == true);
---
+++ ЛАБА1/ЛАБА1.2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Text.Json;
+
+bool ContinueProgramExecutionRequest()
+{
+    Console.WriteLine("Retry the program?(True / False)");
+    bool response = false;
+    try
+    {
+        response = bool.Parse(Console.ReadLine());
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Invalid value entered. Press any key to exit the program.");
+        Console.ReadKey();
+    }
+    Console.Clear();
+    return response;
+}
+
+Dictionary<int,char> DictionaryChanger(Dictionary<int, char> lettersNumbers)
+{   
+    lettersNumbers.Remove(FindMinValue(lettersNumbers));
+    lettersNumbers.Remove(FindMaxValue(lettersNumbers));
+    lettersNumbers = lettersNumbers.OrderBy(x => x.Key).ToDictionary(x => x.Key, x => x.Value);   
+    return lettersNumbers;
+}
+
+#region MinMax
+int FindMinValue(Dictionary<int, char> lettersNumbers)
+{
+    int min = 100, max = -1;
+    foreach(var number in lettersNumbers)
+        min = Math.Min(min, number.Key);
+    return min;
+}
+
+int FindMaxValue(Dictionary<int, char> lettersNumbers)
+{
+    int min = 100, max = -1;
+    foreach (var number in lettersNumbers)
+        max = Math.Max(max, number.Key);
+    return max;
+}
+#endregion
+
+void ProgramExecution()
+{
+    Dictionary <int, char> lettersNumbers = new Dictionary<int,char >()
+    {
+        {5,'e'},
+        {4,'h' },
+        {12,'o' },
+        {8,'l' },
+        {2,'u' },
+        {10,'l' },
+        {17,'m' }
+    };
+
+ Console.WriteLine( JsonSerializer.Serialize(DictionaryChanger(lettersNumbers)));
+}
+
+
+do
+{
+    ProgramExecution();
+}
+while (ContinueProgramExecutionRequest() == true);
