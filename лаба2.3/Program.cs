b41876f5296ba9cd67b447b51da099393d65569f
Dmi-t-ry <93443687+Dmi-t-ry@users.noreply.github.com>
2023-04-19 15:40:28 +0300

    Add files via upload

---
+++ лаба2.3/Program.cs
@@ -0,0 +1,9 @@
+﻿int x = 11, y = 4;
+float z = 4;
+Console.WriteLine(z * y);             // Результат 16
+Console.WriteLine(z * 1e308);     // Рез. "бесконечность"
+Console.WriteLine(x / y);             // Результат 2
+Console.WriteLine(x / z);             // Результат 2,75
+Console.WriteLine(x % y);             // Результат 3
+Console.WriteLine(1e-324 / 1e-324);   // Результат NaN
+Console.ReadKey();
